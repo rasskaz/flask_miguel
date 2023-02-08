defee96d98fb151ad693437b0f8004a3997e548c
rasska <rasska@gmail.com>
2023-02-08 16:41:23 +0700

    mess

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -23,7 +23,7 @@ def index():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me = {}'.format(
-            form.username.data, form.remember_me.data ))
+        flash('Login requested for user test, remember_me = test')
+        #flash('Login requested for user {}, remember_me = {}'.format(form.username.data, form.remember_me.data ))
         return redirect('/index')
     return render_template('login.html', title='Sign in', form=form)
