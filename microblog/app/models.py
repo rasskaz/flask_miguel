da257350c05210fb967b1ebe2783a6bf35a570b1
rasska <rasska@gmail.com>
2023-02-12 14:28:11 +0700

    commit

---
+++ microblog/app/models.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+from werkzeug.security import generate_password_hash,check_password_hash
+from app import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(64), index = True, unique = True)
+    email = db.Column(db.String(120), index = True, unique = True)
+    password_hash = db.Column(db.String(128))
+    posts = db.relationship('Posts', backref='author', lazy='dynamic')
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, pasword):
+        return check_password_hash(self.password_hash, pasword)
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+class Posts(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index = True, default = datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)
+
--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
 
@@ -23,7 +23,6 @@ def index():
 def login():
     form = LoginForm()
     if (form.validate_on_submit()):
-        flash('Login requested for user test, remember_me = test')
-        #flash('Login requested for user {}, remember_me = {}'.format(form.username.data, form.remember_me.data ))
-        return redirect('/index')
-    return render_template('login.html', title='Sign in', form=form)
+        flash('Login requested for user {}, remember_me = {}'.format(form.username.data, form.remember_me.data ))
+        return redirect(url_for('index'))
+    return render_template( 'login.html', title='Sign in', form=form)
